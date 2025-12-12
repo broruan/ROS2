9d3e94c190246954b65baa678b8e6cd0e5f728f9
broruan <3386915695@qq.com>
2025-12-12 22:51:58 +0800

    ROS2基础节点

---
+++ ROS_2/cmake_build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot-max/Desktop/ROS_2
+# Build directory: /home/robot-max/Desktop/ROS_2/cmake_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ROS_2/main.cpp
@@ -0,0 +1,10 @@
+#include"rclcpp/rclcpp.hpp"
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("my_node");
+    RCLCPP_INFO(node->get_logger(), "Hello, ROS2 with C++17!");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
