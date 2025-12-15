429403151bdaf3f04574ca3f3efd628de1db254d
broruan <3386915695@qq.com>
2025-12-15 17:01:06 +0800

    ROS2功能包的构建和使用

--- ROS_2/cmake_build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot-max/Desktop/ROS_2
-# Build directory: /home/robot-max/Desktop/ROS_2/cmake_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
